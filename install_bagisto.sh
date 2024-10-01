7cce44578dcb17694d150a66c70cd92e98a025bc
a.shuklin <a.shuklin@intervolga.ru>
2024-10-01 16:25:39 +0300

    Update install_bagisto.sh

--- install_bagisto.sh
+++ install_bagisto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-composer install && composer update && php artisan migrate --force && php artisan db:seed --force \
+composer install && php artisan migrate --force && php artisan db:seed --force \
   && php artisan cache:clear && php artisan config:clear \
-  && php artisan optimize \
   && yes | php artisan octane:install --server=roadrunner && yes | php artisan octane:install --server=swoole \
-  && php artisan vendor:publish --all --force && php artisan storage:link --force && php artisan key:generate --force
+  && php artisan vendor:publish --all --force && php artisan storage:link --force && php artisan key:generate --force \
+  && php artisan optimize
